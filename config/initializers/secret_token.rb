d59864dbbcff4c178d08d937aa5061d602bb07c2
nazar kuliev <nazar.kuliev@gmail.com>
2011-04-27 23:01:07 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projecthunter::Application.config.secret_token = '6bd4dcb0a82fb31e6fc132d4e7154df79f7dfa6c4c4b5bacf1a26b850fbff5f7d57a8f1cdd150ba923dac26348bd15f5124c8c690bcc264a5b2655796e5cbd73'
