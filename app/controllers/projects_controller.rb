8b1f000040d0b0f205458dc26e9e7d28a2ee220b
nazar kuliev <nazar.kuliev@gmail.com>
2011-05-03 01:02:50 +0600

    bids lower order desc

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.all
+    @projects = Project.where("bids < 5").order("id DESC")
 
     respond_to do |format|
       format.html # index.html.erb
